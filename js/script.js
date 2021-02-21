d2f209b67c45f9d07904bf37bc3b1c214848a662
arturpawlowski5 <66818335+arturpawlowski5@users.noreply.github.com>
2021-02-21 18:22:30 +0700

    Responsive Homepage - Collab - DevChallenges

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Open Mobile Menu
+
+function toggleMobileNavigation() {
+	var element = document.getElementById("mobile-navigation");
+
+	if (element.classList.contains("mobile__open")) {
+		element.classList.remove("mobile__open");
+	} else {
+		element.classList.add("mobile__open");
+	}
+}
